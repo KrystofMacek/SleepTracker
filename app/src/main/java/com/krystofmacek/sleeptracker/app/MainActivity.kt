36cf63122f41aef8dd22b85b1d39c2d8d09b2902
Krystof <krystofmacek333@gmail.com>
2020-06-23 11:01:27 +0200

    Initial commit

---
+++ app/src/main/java/com/krystofmacek/sleeptracker/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.krystofmacek.sleeptracker.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.krystofmacek.sleeptracker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/krystofmacek/sleeptracker/util/Util.kt
@@ -0,0 +1,2 @@
+package com.krystofmacek.sleeptracker.util
+
