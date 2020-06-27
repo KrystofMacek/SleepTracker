235a662d4ba9f77208fa5f68f57ee96475e7fac6
Krystof <krystofmacek333@gmail.com>
2020-06-27 21:08:07 +0200

    Added DiffUtil to check for changes in items of list used in RecyclerView

--- app/src/main/java/com/krystofmacek/sleeptracker/adapters/SleepNightAdapter.kt
+++ app/src/main/java/com/krystofmacek/sleeptracker/adapters/SleepNightAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.krystofmacek.sleeptracker.R
 import com.krystofmacek.sleeptracker.database.SleepNight
@@ -68,6 +69,16 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int {
         return data.size
+    }
+}
+
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
+
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
     }
 
 }
